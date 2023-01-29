efd2b842a90e0ece95c0a58da785a4b95858e73b
DANIELNAHUN <58575264+DANIELNAHUN@users.noreply.github.com>
2023-01-29 18:03:36 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Header/>
+    <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
+            <v-list-item class="px-2">
+                <v-list-item-avatar>
+                    <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-title>Usuario</v-list-item-title>
+
+                <v-btn icon @click.stop="mini = !mini">
+                    <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from '../src/views/HeaderView.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default{
+    components:{
+      Header,
+    },
+    data () {
+      return {
+        drawer: true,
+        items: [
+          { title: 'Home', icon: 'mdi-home-city' },
+          { title: 'My Account', icon: 'mdi-account' },
+          { title: 'Users', icon: 'mdi-account-group-outline' },
+        ],
+        mini: true,
+      }
+    },
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
 </style>
---
+++ src/views/HeaderView.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card class="mx-auto overflow-hidden" height="200px" width="100%">
+        <v-app-bar color="blue-grey darken-4" dark>
+            <!-- <v-app-bar-nav-icon @click.stop="mini = !mini"></v-app-bar-nav-icon> -->
+            <v-spacer></v-spacer>
+            <v-toolbar-title>Title</v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-app-bar>
+    </v-card>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
