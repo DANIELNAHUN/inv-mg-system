0d8f7c2c4ad3fcc36af53e0341ab86f9136434fa
DANIELNAHUN <58575264+DANIELNAHUN@users.noreply.github.com>
2023-01-30 06:31:31 -0500

    test

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,66 @@
 <template>
   <v-app>
-    <Header/>
-    <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
-            <v-list-item class="px-2">
-                <v-list-item-avatar>
-                    <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-                </v-list-item-avatar>
+    <Header />
+    <v-card >
+      <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
+          <v-list-item-title>Usuario</v-list-item-title>
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list dense>
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-card>
+        hola
+      </v-card>
+    </v-card>
+    <v-main>
+      Sy el main
+    </v-main>
 
-                <v-list-item-title>Usuario</v-list-item-title>
-
-                <v-btn icon @click.stop="mini = !mini">
-                    <v-icon>mdi-chevron-left</v-icon>
-                </v-btn>
-            </v-list-item>
-
-            <v-divider></v-divider>
-
-            <v-list dense>
-                <v-list-item v-for="item in items" :key="item.title" link>
-                    <v-list-item-icon>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-item-icon>
-
-                    <v-list-item-content>
-                        <v-list-item-title>{{ item.title }}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view/> -->
   </v-app>
 </template>
 
 <script>
-  import Header from '../src/views/HeaderView.vue'
+import Header from '../src/views/HeaderView.vue'
 
-  export default{
-    components:{
-      Header,
-    },
-    data () {
-      return {
-        drawer: true,
-        items: [
-          { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'My Account', icon: 'mdi-account' },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
-        ],
-        mini: true,
-      }
-    },
-  }
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: 'Home', icon: 'mdi-home-city' },
+        { title: 'My Account', icon: 'mdi-account' },
+        { title: 'Users', icon: 'mdi-account-group-outline' },
+      ],
+      mini: true,
+    }
+  },
+}
 </script>
 
 <style lang="scss">
+
 </style>
--- src/views/HeaderView.vue
+++ src/views/HeaderView.vue
@@ -1,9 +1,9 @@
 <template>
-    <v-card class="mx-auto overflow-hidden" height="200px" width="100%">
+    <v-card class="mx-auto overflow-hidden" height="60" width="100%">
         <v-app-bar color="blue-grey darken-4" dark>
             <!-- <v-app-bar-nav-icon @click.stop="mini = !mini"></v-app-bar-nav-icon> -->
             <v-spacer></v-spacer>
-            <v-toolbar-title>Title</v-toolbar-title>
+            <v-toolbar-title>Sistema de Gestion de Inventario</v-toolbar-title>
             <v-spacer></v-spacer>
         </v-app-bar>
     </v-card>
